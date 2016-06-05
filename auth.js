5244fdc5fbff65c79cb975c7b5304c45cb66882f
Willian Krause <krause.willian@gmail.com>
2016-06-05 12:31:49 -0300

    Chenge in module to use Firebase Auth

--- auth.js
+++ auth.js
@@ -1,280 +1,234 @@
 'use strict';
-(function() {
+(function () {
   angular.module('willcrisis.angular-auth', ['ngRoute', 'ui.router', 'angular-storage'])
-  .provider('authConf', [function () {
-    var options = {
-      loginState: 'login',
-      endpointUrl: '',
-      logoutEndpointUrl: null,
-      usernameProperty: 'username',
-      tokenProperty: 'access_token',
-      rolesProperty: 'roles',
-      refreshTokenProperty: 'refresh_token',
-      tokenTypeProperty: 'token_type',
-      functionIfDenied: function(toState) {
-        $state.go(options.loginState);
-      },
-      functionIfAuthenticated: function(data) {
+    .provider('authConf', [function () {
+      var options = {
+        loginState: 'login',
+        functionIfDenied: function (toState) {
+          $state.go(options.loginState);
+        },
+        functionIfAuthenticated: function (data) {
 
-      },
-      setLoginState: function (state) {
-        options.loginState = state;
-      },
-      setEndpointUrl: function (url) {
-        options.endpointUrl = url;
-      },
-      setFunctionIfDenied: function(functionIfDenied) {
-        options.functionIfDenied = functionIfDenied;
-      },
-      setLogoutEndpointUrl: function(logoutEndpointUrl) {
-        options.logoutEndpointUrl = logoutEndpointUrl;
-      },
-      setUsernameProperty: function(property) {
-          options.usernameProperty = property;
-      },
-      setTokenProperty: function(property) {
-          options.tokenProperty = property;
-      },
-      setRolesProperty: function(property) {
-          options.rolesProperty = property;
-      },
-      setRefreshTokenProperty: function(property) {
-          options.refreshTokenProperty = property;
-      },
-      setTokenTypeProperty: function(property) {
-          options.tokenTypeProperty = property;
-      },
-      setFunctionIfAuthenticated: function(functionIfAuthenticated) {
+        },
+        functionIfLoggedOff: function () {
+
+        },
+        setLoginState: function (state) {
+          options.loginState = state;
+        },
+        setFunctionIfDenied: function (functionIfDenied) {
+          options.functionIfDenied = functionIfDenied;
+        },
+        setFunctionIfAuthenticated: function (functionIfAuthenticated) {
           options.functionIfAuthenticated = functionIfAuthenticated;
-      }
-    };
-
-    this.$get = [function () {
-      if (!options) {
-        throw new Error('Não foi possível carregar as configurações.');
-      }
-      return options;
-    }];
-  }])
-  .service('auth', ['$rootScope', 'store', '$http', 'authConf', function ($rootScope, store, $http, authConf) {
-    this.username = null;
-    this.roles = null;
-    this.token = null;
-    this.refreshToken = null;
-    this.tokenType = null;
-    this.loggedIn = false;
-
-    var service = this;
-
-    this.broadcast = function () {
-      $rootScope.$broadcast('userChange');
-    };
-
-    this.login = function (username, password) {
-      var data = {username: username, password: password};
-      return Promise.race([
-        $http.post(authConf.endpointUrl, data)
-          .then(
-            function (result) {
-              service.authenticate(result.data);
-              return service;
-            },
-            function (error) {
-              throw error;
-            }
-          )
-      ]);
-    };
-
-    this.authenticate = function (data) {
-      setData(service, data);
-      $http.defaults.headers.common.Authorization = (this.tokenType + " " + this.token).trim();
-      authConf.functionIfAuthenticated(data);
-      store.set('auth', data);
-    };
-
-    this.logout = function () {
-      setData(service, {});
-      $http.defaults.headers.common.Authorization = null;
-      store.remove('auth');
-      if (authConf.logoutEndpointUrl) {
-          return $http.get(authConf.logoutEndpointUrl);
-      }
-    };
-
-    this.hasRole = function (role) {
-      if (!this.roles) {
-        return false;
-      }
-      return this.roles.indexOf(role) > -1;
-    };
-
-    this.hasAllRoles = function (roles) {
-      if (!this.roles) {
-        return false;
-      }
-      for (var i = 0; i < roles.length; i++) {
-        if (!this.hasRole(roles[i])) {
-          return false;
+        },
+        setFunctionIfLoggedOff: function (functionIfLoggedOff) {
+          options.functionIfLoggedOff = functionIfLoggedOff;
         }
-      }
-      return true;
-    };
+      };
 
-    this.hasAnyRole = function (roles) {
-      if (!this.roles) {
-        return false;
+      this.$get = [function () {
+        if (!options) {
+          throw new Error('Não foi possível carregar as configurações.');
+        }
+        return options;
+      }];
+    }])
+    .service('auth', ['$rootScope', 'store', '$http', 'authConf', function ($rootScope, store, $http, authConf) {
+      if (!firebase) {
+        throw new Error('Firebase is not defined! Please follow these instructions for configuring it in your project: https://firebase.google.com/docs/web/setup')
       }
-      for (var i = 0; i < roles.length; i++) {
-        if (this.hasRole(roles[i])) {
+      this.firebaseAuth = firebase.auth();
+
+      this.uid = null;
+      this.email = null;
+      this.displayName = null;
+      this.photoURL = null;
+      this.token = null;
+      this.refreshToken = null;
+      this.loggedIn = false;
+
+      var service = this;
+
+      this.broadcast = function () {
+        $rootScope.$broadcast('userChange');
+      };
+
+      this.loginWithGoogle = function () {
+        return loginWithProvider(getProviderForProviderId('google.com'));
+      };
+
+      this.loginWithFacebook = function () {
+        return loginWithProvider(getProviderForProviderId('facebook.com'));
+      };
+
+      this.loginWithTwitter = function () {
+        return loginWithProvider(getProviderForProviderId('twitter.com'));
+      };
+
+      this.authenticate = function (data) {
+        setData(data);
+        authConf.functionIfAuthenticated(data);
+        store.set('auth', {
+          user: {
+            uid: data.user.uid,
+            email: data.user.email,
+            displayName: data.user.displayName,
+            photoURL: data.user.photoURL,
+            refreshToken: data.user.refreshToken
+          }, credential: {
+            idToken: data.credential.idToken
+          }
+        });
+      };
+
+      this.logout = function () {
+        return Promise.race([
+          service.firebaseAuth.signOut().then(function () {
+            setData({});
+            authConf.functionIfLoggedOff();
+            store.remove('auth');
+          })
+        ]);
+      };
+
+      this.canAccess = function (state) {
+        if (!state || state.auth === undefined) {
           return true;
-        }
-      }
-      return false;
-    };
-
-    this.canAccess = function (state) {
-      if (!state) {
-        return true;
-      }
-      if (state.auth === undefined) {
-        return true;
-      } else if (state.auth.constructor == Array) {
-        if (state.requireAll) {
-          return this.hasAllRoles(state.auth);
+        } else if (state.auth.constructor == Array) {
+          console.log('Implementar lógica');
+          return true;
         } else {
-          return this.hasAnyRole(state.auth);
+          return state.auth === this.loggedIn
         }
-      } else {
-        return state.auth === this.loggedIn
-      }
-    };
+      };
 
-    function setData(service, response) {
-      service.username = response[authConf.usernameProperty];
-      service.token = response[authConf.tokenProperty];
-      service.roles = response[authConf.rolesProperty];
-      service.refreshToken = response[authConf.refreshTokenProperty];
-      service.tokenType = response[authConf.tokenTypeProperty];
-      service.loggedIn = !!response[authConf.tokenProperty];
-    }
-  }])
-  .directive('authUsername', ['auth', function (auth) {
-    return {
-      restrict: 'E',
-      template: '{{username}}',
-      controller: ['$scope', function($scope) {
-          $scope.username = auth.username;
-      }]
-    }
-  }])
-  .directive('authLoggedIn', ['auth', 'ngIfDirective', function (auth, ngIfDirective) {
-    var ngIf = ngIfDirective[0];
-    return {
-      restrict: 'AE',
-      transclude: ngIf.transclude,
-      priority: ngIf.priority - 1,
-      terminal: ngIf.terminal,
-      link: function (scope, element, attrs) {
-        attrs.ngIf = function() {
-          return auth.loggedIn;
-        };
-        ngIf.link.apply(ngIf, arguments);
+      function loginWithProvider(provider) {
+        return Promise.race([
+          service.firebaseAuth.signInWithPopup(provider).then(function (result) {
+            service.authenticate(result);
+            return service;
+          }).catch(function (error) {
+            handleError(error);
+          })
+        ]);
       }
-    }
-  }])
-  .directive('authNotLoggedIn', ['auth', 'ngIfDirective', function (auth, ngIfDirective) {
-    var ngIf = ngIfDirective[0];
-    return {
-      restrict: 'AE',
-      transclude: ngIf.transclude,
-      priority: ngIf.priority - 1,
-      terminal: ngIf.terminal,
-      link: function (scope, element, attrs) {
-        attrs.ngIf = function() {
-          return !auth.loggedIn;
-        };
-        ngIf.link.apply(ngIf, arguments);
-      }
-    }
-  }])
-  .directive('authHasRole', ['auth', 'ngIfDirective', function (auth, ngIfDirective) {
-    var ngIf = ngIfDirective[0];
-    return {
-      restrict: 'AE',
-      transclude: ngIf.transclude,
-      priority: ngIf.priority - 1,
-      terminal: ngIf.terminal,
-      scope: {
-        role: '@'
-      },
-      link: function (scope, element, attrs) {
-        var value = scope.role || attrs.authHasRole;
-        if (!value) {
-          throw new Error('auth-has-role: É necessário informar uma Role');
-        }
-        attrs.ngIf = function() {
-          return auth.hasRole(value);
-        };
-        ngIf.link.apply(ngIf, arguments);
-      }
-    }
-  }])
-  .directive('authHasAnyRole', ['auth', 'ngIfDirective', function (auth, ngIfDirective) {
-    var ngIf = ngIfDirective[0];
-    return {
-      restrict: 'AE',
-      transclude: ngIf.transclude,
-      priority: ngIf.priority - 1,
-      terminal: ngIf.terminal,
-      scope: {
-        roles: '@'
-      },
-      link: function (scope, element, attrs) {
-        var value = scope.roles ? scope.roles.split(',') : attrs.authHasAnyRole ? attrs.authHasAnyRole.split(',') : null;
-        if (!value) {
-          throw new Error('auth-has-any-role: É necessário informar pelo menos uma Role');
-        }
-        attrs.ngIf = function() {
-          return auth.hasAnyRole(value);
-        };
-        ngIf.link.apply(ngIf, arguments);
-      }
-    }
-  }])
-  .directive('authHasAllRoles', ['auth', 'ngIfDirective', function (auth, ngIfDirective) {
-    var ngIf = ngIfDirective[0];
-    return {
-      restrict: 'AE',
-      transclude: ngIf.transclude,
-      priority: ngIf.priority - 1,
-      terminal: ngIf.terminal,
-      scope: {
-        roles: '@'
-      },
-      link: function (scope, element, attrs) {
-        var value = scope.roles ? scope.roles.split(',') : attrs.authHasAllRoles ? attrs.authHasAllRoles.split(',') : null;
-        if (!value) {
-          throw new Error('auth-has-all-roles: É necessário informar pelo menos uma Role');
-        }
-        attrs.ngIf = function() {
-          return auth.hasAllRoles(value);
-        };
-        ngIf.link.apply(ngIf, arguments);
-      }
-    }
-  }])
-  .run(['$rootScope', '$state', 'auth', 'authConf', 'store', function($rootScope, $state, auth, authConf, store) {
-    $rootScope.$on('$stateChangeStart', function (event, toState) {
-      if (!auth.canAccess(toState)) {
-        event.preventDefault();
-        authConf.functionIfDenied(toState);
-      }
-    });
 
-    var response = store.get('auth') || {};
-    if (response) {
-      auth.authenticate(response);
-    }
-  }]);
+      function setData(response) {
+        response.user = response.user || {};
+        response.credential = response.credential || {};
+
+        service.uid = response.user.uid;
+        service.email = response.user.email;
+        service.displayName = response.user.displayName;
+        service.photoURL = response.user.photoURL;
+        service.token = response.credential.idToken;
+        service.refreshToken = response.user.refreshToken;
+        service.loggedIn = !!response.user.uid;
+      }
+
+      function handleError(error) {
+        if (error.code === 'auth/account-exists-with-different-credential') {
+          var pendingCred = error.credential;
+          var email = error.email;
+
+          service.firebaseAuth.fetchProvidersForEmail(email).then(function (providers) {
+            var provider = getProviderForProviderId(providers[0]);
+            service.firebaseAuth.signInWithPopup(provider).then(function(result) {
+              result.user.link(pendingCred).then(function() {
+                service.authenticate(result);
+                return new Promise(function(resolve, reject) {
+                  resolve();
+                });
+              });
+            });
+          });
+        } else {
+          throw error;
+        }
+      }
+
+      function getProviderForProviderId(providerId) {
+        switch (providerId) {
+          case 'google.com':
+            return new firebase.auth.GoogleAuthProvider();
+          case 'facebook.com':
+            return new firebase.auth.FacebookAuthProvider();
+          case 'twitter.com':
+            return new firebase.auth.TwitterAuthProvider();
+          default:
+            return null;
+        }
+      }
+    }])
+    .directive('authEmail', ['auth', function (auth) {
+      return {
+        restrict: 'E',
+        template: '{{email}}',
+        controller: ['$scope', function ($scope) {
+          $scope.email = auth.email;
+        }]
+      }
+    }])
+    .directive('authDisplayName', ['auth', function (auth) {
+      return {
+        restrict: 'E',
+        template: '{{displayName}}',
+        controller: ['$scope', function ($scope) {
+          $scope.displayName = auth.displayName;
+        }]
+      }
+    }])
+    .directive('authUserPhoto', ['auth', function (auth) {
+      return {
+        restrict: 'E',
+        template: '<img ng-src="{{photoURL}}" />',
+        controller: ['$scope', function ($scope) {
+          $scope.photoURL = auth.photoURL;
+        }]
+      }
+    }])
+    .directive('authLoggedIn', ['auth', 'ngIfDirective', function (auth, ngIfDirective) {
+      var ngIf = ngIfDirective[0];
+      return {
+        restrict: 'AE',
+        transclude: ngIf.transclude,
+        priority: ngIf.priority - 1,
+        terminal: ngIf.terminal,
+        link: function (scope, element, attrs) {
+          attrs.ngIf = function () {
+            return auth.loggedIn;
+          };
+          ngIf.link.apply(ngIf, arguments);
+        }
+      }
+    }])
+    .directive('authNotLoggedIn', ['auth', 'ngIfDirective', function (auth, ngIfDirective) {
+      var ngIf = ngIfDirective[0];
+      return {
+        restrict: 'AE',
+        transclude: ngIf.transclude,
+        priority: ngIf.priority - 1,
+        terminal: ngIf.terminal,
+        link: function (scope, element, attrs) {
+          attrs.ngIf = function () {
+            return !auth.loggedIn;
+          };
+          ngIf.link.apply(ngIf, arguments);
+        }
+      }
+    }])
+    .run(['$rootScope', '$state', 'auth', 'authConf', 'store', function ($rootScope, $state, auth, authConf, store) {
+      $rootScope.$on('$stateChangeStart', function (event, toState) {
+        if (!auth.canAccess(toState)) {
+          event.preventDefault();
+          authConf.functionIfDenied(toState);
+        }
+      });
+
+      var response = store.get('auth') || {};
+      if (response) {
+        auth.authenticate(response);
+      }
+    }]);
 })();
